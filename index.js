ed828b256bd397265f24a62b923d6c5783551a07
Amrit <amritsinghal2211@gmail.com>
2024-05-01 18:33:51 +0530

    hi

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const { createServer } = require('node:http');
-
+const { join } = require('node:path');
+const {Server} = require('socket.io')
 const app = express();
 const server = createServer(app);
-
+const io = new Server(server)
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
 });
-
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
